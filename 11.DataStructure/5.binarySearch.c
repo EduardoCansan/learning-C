254af3e8753206e685f91e8ea8cdd21c107234e0
Eduardo Cansan <eduardocansann@gmail.com>
2025-08-02 18:13:52 -0300

    fix: add steps searchs

--- 11.DataStructure/5.binarySearch.c
+++ 11.DataStructure/5.binarySearch.c
@@ -1,17 +1,19 @@
+/*
+    Write a program in C to create a Binary Search
+*/
+
 #include <stdio.h>
 #define SIZE 20
 
 int binarySearch(int array[], int key, int size);
 
 int main() {
- 
+
     int array[SIZE] = {1, 4, 7, 8, 10, 14, 19, 21, 25, 26, 27, 30, 40, 41, 43, 37, 51, 53, 55, 58};
-    
+
     int key = 53;
 
-    int ret = binarySearch(array, key, SIZE);
-
-    printf("The element %d it is in position %d", key, (ret + 1));
+    binarySearch(array, key, SIZE);
 
     return 0;
 }
@@ -19,13 +21,18 @@ int main() {
 int binarySearch(int array[], int key, int size) {
     int start = 0;
     int end = size - 1;
+    int step = 0;
 
     while(start <= end) {
+        
+        step++;
+        
         // Find the mid of array
         int mid = (start + end) / 2;
 
         // if key is in the middle
         if(key == array[mid]) {
+            printf("The element %d is in position %d and took %d steps.", key, mid, step);
             return mid;
         }
 
@@ -37,5 +44,6 @@ int binarySearch(int array[], int key, int size) {
         }
     }
 
+    printf("The element %d is not in the list, %d steps taken.", key, step);
     return -1;
 }
--- 11.DataStructure/6.linearSearch.c
+++ 11.DataStructure/6.linearSearch.c
@@ -1,29 +1,35 @@
+/*
+    Write a program in C to create a Linear Search
+*/
+
 #include <stdio.h>
 #define SIZE 20
 
 int linearSearch(int array[], int key, int size);
 
 int main() {
- 
+
     int array[SIZE] = {1, 4, 7, 8, 10, 14, 19, 21, 25, 26, 27, 30, 40, 41, 43, 37, 51, 53, 55, 58};
-    
+
     int key = 53;
 
-    int ret = linearSearch(array, key, SIZE);
-
-    printf("The element %d it is in position %d", key, (ret + 1));
+    linearSearch(array, key, SIZE);
 
     return 0;
 }
 
 int linearSearch(int array[], int key, int size) {
     int i;
+    int step = 0;
 
     for(i = 0; i < size; i++) {
+        step++;
         if(array[i] == key) {
+            printf("The element %d is in position %d and took %d steps.", key, i, step);
             return i;
         }
     }
 
+    printf("The element %d is not in the list, %d steps taken.", key, step);
     return -1;
 }
