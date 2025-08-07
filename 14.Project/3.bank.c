40f01dcdc3e558d92cc2eade053a1eaad7d42797
Eduardo Cansan <eduardocansann@gmail.com>
2025-08-07 17:34:41 -0300

    fix: bank project logic

--- 14.Project/3.bank.c
+++ 14.Project/3.bank.c
@@ -1,12 +1,3 @@
-/*
-    Create an application that, on startup, presents a menu of actions the user can perform in a bank:
-        Create a new accoun, Make a withdrawal, Make a deposit, Make a transfer, 
-        List all accounts, Exit the system
-
-    The system should handle each operation appropriately and display clear messages 
-    to the user after each action.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,7 +18,6 @@ typedef struct {
     char cpf[CLIENT_CPF_SIZE];
     char dateBirth[CLIENT_BIRTH_SIZE];
     char registerDate[CLIENT_DATE_SIZE];
-
 } Client;
 
 typedef struct {
@@ -35,8 +25,7 @@ typedef struct {
     Client client;
     float balance;
     float limit;
-    float totalBalence; // balance + limit
-
+    float totalBalence;
 } Account;
 
 void menu();
@@ -56,18 +45,14 @@ void transfer(Account origin_account, Account destiny_account, float value);
 static Account accounts[ACCOUNT_DATA_SIZE];
 static int account_count = 0;
 static int client_count = 0;
-
 int i;
 
 int main() {
-
     menu();
-    
     return 0;
 }
 
 void menu() {
-
     int option;
     printf("\n==============================\n");
     printf("============ BANK ============\n");
@@ -85,101 +70,71 @@ void menu() {
     scanf("%d", &option);
 
     switch (option) {
-    case 1:
-        createAccount();
-        break;
-    case 2:
-        makeWithdrawal();
-        break;
-    case 3:
-        makeDeposit();
-        break;
-    case 4:
-        makeDeposit();
-        break;
-    case 5:
-        listAccounts();
-        break;
-    case 6:
-        printf("Thank you for your trust!\n");
-        Sleep(2000);
-        exit(0);
-
-    default:
-        printf("*** Invalid Option ***\n");
-        Sleep(2000);
-        menu();
+        case 1:
+            createAccount();
+            break;
+        case 2:
+            makeWithdrawal();
+            break;
+        case 3:
+            makeDeposit();
+            break;
+        case 4:
+            makeTransfer();
+            break;
+        case 5:
+            listAccounts();
+            break;
+        case 6:
+            printf("Thank you for your trust!\n");
+            Sleep(2000);
+            exit(0);
+        default:
+            printf("*** Invalid Option ***\n");
+            Sleep(2000);
+            menu();
     }
 }
 
 void infoClient(Client client) {
-    printf("Code: %d, \nName: %s \nDate of birth: %s \nRegister: %s\n",
-    strtok(client.name, "\n"), strtok(client.dateBirth, "\n"), strtok(client.dateBirth, "\n"));
+    printf("Name: %sDate of birth: %sRegister: %s\n",
+           client.name, client.dateBirth, client.registerDate);
 }
 
 void infoAccount(Account account) {
-    printf("Account number: %s \nClient: %s \nDate of birth: %s \nRegister Date: %s \nTotal Balance: %2.f\n",
-    account.number, strtok(account.client.name, "\n"), strtok(account.client.dateBirth, "\n"), 
-    strtok(account.client.registerDate, "\n"), account.totalBalence);
+    printf("\nAccount number: %d\nClient: %s\nDate of birth: %s\nRegister Date: %s\nTotal Balance: %.2f\n",
+           account.number, account.client.name, account.client.dateBirth,
+           account.client.registerDate, account.totalBalence);
 }
 
 void createAccount() {
     Client client;
 
-    // Registration date
-    char day[3];
-    char month[3];
-    char year[5];
+    // Date
     char registration_date[20];
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
 
-    // day
-    if(tm.tm_mday < 10) {
-        sprintf(day, "0%d", tm.tm_mday); // transform number in 1 to 01, till 10
-    
-    } else {
-        sprintf(day, "%d", tm.tm_mday);
-    }
-
-    // month
-    if((tm.tm_mon + 1) < 10) { // +1 cause goes 0 to 11 (not 1 to 12)
-        sprintf(month, "0%d", tm.tm_mon + 1);
-    
-    } else {
-        sprintf(month, "%d", tm.tm_mon + 1);
-    }
-
-    //year
-    sprintf(year, "%d", tm.tm_year + 1900); // default (current year - 1900)
-
-    strcpy(registration_date, "");
-    strcat(registration_date, day);
-    strcat(registration_date, "/");
-    strcat(registration_date, month);
-    strcat(registration_date, "/");
-    strcat(registration_date, year);
-    strcat(registration_date, "\0"); // finish
+    sprintf(registration_date, "%02d/%02d/%d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
     strcpy(client.registerDate, registration_date);
 
-    //Creating Client
-    printf("Enter Client Data: \n");
+    printf("\nEnter Client Data:\n");
 
-    printf("\nClient Name: \n");
-    fgets(client.name, CLIENT_NAME_SIZE, stdin);
+    printf("Client Name: ");
+    scanf(" %[^\n]s", client.name);
 
-    printf("Client Email: \n");
-    fgets(client.email, CLIENT_EMAIL_SIZE, stdin);
+    printf("Client Email: ");
+    scanf(" %[^\n]s", client.email);
 
-    printf("Client CPF: \n");
-    fgets(client.cpf, CLIENT_CPF_SIZE, stdin);
+    printf("Client CPF: ");
+    scanf(" %[^\n]s", client.cpf);
 
-    printf("Client Date Birth: \n");
-    fgets(client.dateBirth, CLIENT_BIRTH_SIZE, stdin);
+    printf("Client Date of Birth: ");
+    scanf(" %[^\n]s", client.dateBirth);
 
     client_count++;
 
-    // Creating Account
+    // Create Account
     accounts[account_count].number = account_count + 1;
     accounts[account_count].client = client;
     accounts[account_count].balance = 0.0;
@@ -187,21 +142,21 @@ void createAccount() {
     accounts[account_count].totalBalence = updateTotalBalance(accounts[account_count]);
 
     printf("\nAccount Created Successfully!\n");
-    printf("\nAccount Data: \n");
+    printf("Account Data:\n");
     infoAccount(accounts[account_count]);
     account_count++;
 
-    Sleep(5000);
+    Sleep(3000);
+    menu();
 }
 
 void listAccounts() {
-    if(account_count > 0) {
-        for(i = 0; i < account_count; i++) {
+    if (account_count > 0) {
+        for (i = 0; i < account_count; i++) {
             infoAccount(accounts[i]);
             printf("\n");
             Sleep(1000);
         }
-
     } else {
         printf("***There are no registered accounts yet***\n");
     }
@@ -214,45 +169,44 @@ float updateTotalBalance(Account account) {
 }
 
 Account searchAccountByNumber(int number) {
-    Account a;
-    if(account_count > 0) {
-        for (i = 0; i < account_count; i++) {
-            if(accounts[i].number == number) {
-                a = accounts[i];
-            }
-        }    
+    for (i = 0; i < account_count; i++) {
+        if (accounts[i].number == number) {
+            return accounts[i];
+        }
     }
-    return a;
+
+    Account empty = {0};
+    return empty;
 }
 
 void withdrawal(Account account, float value) {
-    if(value > 0 && account.totalBalence >= value) {
-        for(i = 0; i < account_count; i++) {
-            if(accounts[i].number == account.number) {
-                if(accounts[i].balance >= value) {
-                    accounts[i].balance = accounts[i].balance - value;
-                    accounts[i].totalBalence = updateTotalBalance(accounts[i]);
-                    printf("Withdrawal made successfully\n");
-                
+    if (value > 0 && account.totalBalence >= value) {
+        for (i = 0; i < account_count; i++) {
+            if (accounts[i].number == account.number) {
+                if (accounts[i].balance >= value) {
+                    accounts[i].balance -= value;
                 } else {
-                    float remaining = accounts[i].balance - value;
-                    accounts[i].limit = accounts[i].limit - remaining;
+                    float remaining = value - accounts[i].balance;
                     accounts[i].balance = 0.0;
-                    accounts[i].totalBalence = updateTotalBalance(accounts[i]);
-                    printf("Withdrawal made successfully\n");
+                    accounts[i].limit -= remaining;
                 }
+                accounts[i].totalBalence = updateTotalBalance(accounts[i]);
+                printf("Withdrawal made successfully\n");
             }
         }
     } else {
         printf("***Withdrawal not made***\n");
     }
+
+    Sleep(2000);
+    menu();
 }
 
 void deposit(Account account, float value) {
-    if(value > 0) {
-        for(i = 0; i < account_count; i++) {
-            if(accounts[i].number == account.number) {
-                accounts[i].balance = accounts[i].balance + value;
+    if (value > 0) {
+        for (i = 0; i < account_count; i++) {
+            if (accounts[i].number == account.number) {
+                accounts[i].balance += value;
                 accounts[i].totalBalence = updateTotalBalance(accounts[i]);
                 printf("Deposit made successfully!\n");
             }
@@ -261,34 +215,30 @@ void deposit(Account account, float value) {
         printf("***Deposit not made***\n");
     }
 
+    Sleep(2000);
+    menu();
 }
 
 void transfer(Account origin_account, Account destiny_account, float value) {
-    int oa, da; //oa => origin account // da => destiny account
-    
-    if(value > 0 && origin_account.totalBalence >= value) {
-        for(oa = 0; oa < account_count; oa++) {
-            if(accounts[oa].number == origin_account.number) {
-                for(da = 0; da < account_count; da++) {
-                    if(accounts[da].number == destiny_account.number) {
-                        if(accounts[oa].balance >= value) {
+    int oa, da;
 
-                            accounts[oa].balance = accounts[oa].balance - value;
-                            accounts[da].balance = accounts[da].balance + value;
-
-                            accounts[oa].totalBalence = updateTotalBalance(accounts[oa]);
-                            accounts[da].totalBalence = updateTotalBalance(accounts[da]);
-                            printf("Transfer made successfully!\n");
-                        
+    if (value > 0 && origin_account.totalBalence >= value) {
+        for (oa = 0; oa < account_count; oa++) {
+            if (accounts[oa].number == origin_account.number) {
+                for (da = 0; da < account_count; da++) {
+                    if (accounts[da].number == destiny_account.number) {
+                        if (accounts[oa].balance >= value) {
+                            accounts[oa].balance -= value;
                         } else {
-                            float remaining = accounts[oa].balance - value;
-                            accounts[oa].limit = accounts[oa].limit + remaining;
+                            float remaining = value - accounts[oa].balance;
                             accounts[oa].balance = 0.0;
-                            accounts[da].balance = accounts[da].balance + value;
-                            accounts[oa].totalBalence = updateTotalBalance(accounts[oa]);
-                            accounts[da].totalBalence = updateTotalBalance(accounts[da]);
-                            printf("Transfer made successfully!\n");
+                            accounts[oa].limit -= remaining;
                         }
+                        accounts[da].balance += value;
+
+                        accounts[oa].totalBalence = updateTotalBalance(accounts[oa]);
+                        accounts[da].totalBalence = updateTotalBalance(accounts[da]);
+                        printf("Transfer made successfully!\n");
                     }
                 }
             }
@@ -297,27 +247,29 @@ void transfer(Account origin_account, Account destiny_account, float value) {
         printf("***Transfer not made***\n");
     }
 
+    Sleep(2000);
+    menu();
 }
 
 void makeWithdrawal() {
-    if(account_count > 0) {
+    if (account_count > 0) {
         int number;
-        printf("Enter the account number: \n");
+        printf("Enter the account number: ");
         scanf("%d", &number);
 
         Account account = searchAccountByNumber(number);
 
-        if(account.number == number) {
+        if (account.number == number) {
             float value;
-            printf("Inform the withdrawal amount: \n");
-            scanf("%d", &value);
+            printf("Enter the withdrawal amount: ");
+            scanf("%f", &value);
 
             withdrawal(account, value);
-        
         } else {
-            printf("No account was found with the number %d\n", number);
+            printf("No account found with number %d\n", number);
+            Sleep(2000);
+            menu();
         }
-    
     } else {
         printf("***There are no accounts for withdrawals yet***\n");
         Sleep(2000);
@@ -326,24 +278,24 @@ void makeWithdrawal() {
 }
 
 void makeDeposit() {
-    if(account_count > 0) {
+    if (account_count > 0) {
         int number;
-        printf("Enter the account number: \n");
+        printf("Enter the account number: ");
         scanf("%d", &number);
 
         Account account = searchAccountByNumber(number);
 
-        if(account.number == number) {
+        if (account.number == number) {
             float value;
-            printf("Inform the deposit amount: \n");
-            scanf("%d", &value);
+            printf("Enter the deposit amount: ");
+            scanf("%f", &value);
 
             deposit(account, value);
-        
         } else {
-            printf("No account was found with the number %d\n", number);
+            printf("No account found with number %d\n", number);
+            Sleep(2000);
+            menu();
         }
-    
     } else {
         printf("***There are no accounts for deposit yet***\n");
         Sleep(2000);
@@ -352,38 +304,38 @@ void makeDeposit() {
 }
 
 void makeTransfer() {
-    if(account_count > 0) {
+    if (account_count > 0) {
         int origin_num, destiny_num;
-        printf("Enter your account number: \n");
-        scanf("%d", origin_num);
+        printf("Enter your account number: ");
+        scanf("%d", &origin_num);
 
         Account origin_account = searchAccountByNumber(origin_num);
 
-        if(origin_account.number == origin_num) {
-            printf("Enter the destination account number: \n");
+        if (origin_account.number == origin_num) {
+            printf("Enter the destination account number: ");
             scanf("%d", &destiny_num);
 
             Account destiny_account = searchAccountByNumber(destiny_num);
 
-            if(destiny_account.number == destiny_num) {
+            if (destiny_account.number == destiny_num) {
                 float value;
-                printf("Inform the amount for the transfer: \n");
+                printf("Enter the transfer amount: ");
                 scanf("%f", &value);
-                
+
                 transfer(origin_account, destiny_account, value);
-
             } else {
-                printf("The destination account with number %d was not found\n", destiny_num);
+                printf("Destination account with number %d not found\n", destiny_num);
+                Sleep(2000);
+                menu();
             }
-
         } else {
-            printf("The account with number %d was not found\n", origin_num);
+            printf("Account with number %d not found\n", origin_num);
+            Sleep(2000);
+            menu();
         }
-
-
     } else {
         printf("***There are no accounts for transfer yet***\n");
+        Sleep(2000);
+        menu();
     }
-    Sleep(2000);
-    menu();
 }
