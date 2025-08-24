c4145726991f761a479ea7150113294ec0b98f1f
Eduardo Cansan <eduardocansann@gmail.com>
2025-08-24 13:49:47 -0300

    fix: comments to help undestand scanf

--- 1.Introduction/3.scan.c
+++ 1.Introduction/3.scan.c
@@ -9,9 +9,12 @@ int main() {
     int number;
 
     printf("Digit a number: ");
+    // Scanf uses the keyboard to input something
     scanf("%d", &number);
+    // first you set the type of variable
+    // that insert the variable with a '&' to store the value in the variable
     
-    printf("You chose the number %d!", number);
+    printf("You chose the number %d", number);
 
     return 0;
 }
